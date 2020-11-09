54088bb7ee480f2952d6ff3e1895ef4857919c9d
Lucas Soto <lucaslsoto95@gmail.com>
2020-11-09 13:30:45 -0300

    [UPD] crm_extension: bring latest changes from odoo-calyx

--- crm_extension/models/lead.py
+++ crm_extension/models/lead.py
@@ -2,15 +2,11 @@
 # For copyright and license notices, see __manifest__.py file in module root
 # directory
 ##############################################################################
-from odoo import models, fields, api, _
-from odoo.exceptions import ValidationError
-from dateutil.relativedelta import relativedelta
-import re
-import logging
-_logger = logging.getLogger(__name__)
+
+from odoo import models, fields, _
 
 
 class CrmLead(models.Model):
     _inherit = "crm.lead"
 
-    requested_date = fields.Date(string='Requested Date',)
+    requested_date = fields.Datetime(string='Requested Date')
